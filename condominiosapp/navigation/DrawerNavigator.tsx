fc0c622c49c24ce5a4e37939ac1b1e3b77e71c01
Nicolas <nicolasuliana10@gmail.com>
2025-06-26 01:06:36 -0300

    feat: add condominium routes to Drawer navigation

--- condominiosapp/navigation/DrawerNavigator.tsx
+++ condominiosapp/navigation/DrawerNavigator.tsx
@@ -4,35 +4,63 @@ import React from 'react';
 import CustomDrawerContent from '../components/CustomDrawerContent';
 import HomeScreen from '../screens/HomeScreen';
 
+// Condominios
+import CondominiumScreen from '../screens/condominiums/CondominiumScreen';
+import CreateCondominiumScreen from '../screens/condominiums/CreateCondominiumScreen';
+import EditCondominiumScreen from '../screens/condominiums/EditCondominiumScreen';
+
 export type DrawerParamList = {
-  Home: undefined;
+    Home: undefined;
+    Condominios: undefined;
+    CreateCondominio: undefined;
+    EditCondominio: { id: number, nome: string, endereco: string, cnpj: string, quantidade_blocos: number };
 };
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator
-      initialRouteName="Home"
-      drawerContent={(props) => <CustomDrawerContent {...props} />}
-      screenOptions={{
-        drawerActiveTintColor: '#4B7BE5',
-        drawerLabelStyle: { marginLeft: 0, fontSize: 16 },
-        drawerStyle: { backgroundColor: '#fff', width: 250 },
-        headerStyle: { backgroundColor: '#4B7BE5' },
-        headerTintColor: '#fff',
-      }}
-    >
-      <Drawer.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          drawerIcon: ({ color, size }) => <Ionicons name="home-outline" size={size} color={color} />,
-          title: 'Início',
-        }}
-      />
-    </Drawer.Navigator>
-  );
+    return (
+        <Drawer.Navigator
+            initialRouteName="Home"
+            drawerContent={(props) => <CustomDrawerContent {...props} />}
+            screenOptions={{
+                drawerActiveTintColor: '#4B7BE5',
+                drawerLabelStyle: { marginLeft: 0, fontSize: 16 },
+                drawerStyle: { backgroundColor: '#fff', width: 250 },
+                headerStyle: { backgroundColor: '#4B7BE5' },
+                headerTintColor: '#fff',
+            }}
+        >
+            <Drawer.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    drawerIcon: ({ color, size }) => <Ionicons name="home-outline" size={size} color={color} />,
+                    title: 'Início',
+                }}
+            />
+            <Drawer.Screen
+                name="Condominios"
+                component={CondominiumScreen}
+                options={{
+                    drawerIcon: ({ color, size }) => <Ionicons name="business" size={size} color={color} />,
+                    title: 'Condomínios',
+                }}
+            />
+
+            <Drawer.Screen
+                name="CreateCondominio"
+                component={CreateCondominiumScreen}
+                options={{ drawerItemStyle: { display: 'none' }, title: 'Novo Condomínio' }}
+            />
+
+            <Drawer.Screen
+                name="EditCondominio"
+                component={EditCondominiumScreen}
+                options={{ drawerItemStyle: { display: 'none' }, title: 'Editar Condomínio' }}
+            />
+        </Drawer.Navigator>
+    );
 };
 
 export default DrawerNavigator;
